ea0aae551a953868cf290cbdaa2d38ab7cc4f3ea
arpitjhaveri <40744834+arpitjhaveri@users.noreply.github.com>
2026-02-19 23:19:27 -0500

    Implement Bollinger Breakout strategy class

---
+++ strategies/bollinger_breakout.py
@@ -0,0 +1,13 @@
+import pandas_ta as ta
+from .base import StrategyBase
+
+class BollingerBreakout(StrategyBase):
+    def generate_signal(self, df, params):
+        bb = ta.bbands(df["close"], length=params.get("period", 20), std=params.get("std", 2.0))
+        df = pd.concat([df, bb], axis=1)
+        latest = df.iloc[-1]
+        if latest["close"] > latest["BBU_20_2.0"]:
+            return "BUY"
+        elif latest["close"] < latest["BBL_20_2.0"]:
+            return "SELL"
+        return "HOLD"
